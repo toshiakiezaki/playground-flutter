d74ad4af73af5a55a18954731743ca085685e3aa
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2024-03-18 01:21:05 -0300

    Initial structure for authentication playground

---
+++ authentication/lib/adapters/interfaces/pages/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: const SafeArea(
+        child: Text("Hello world"),
+      ),
+    );
+  }
+}
