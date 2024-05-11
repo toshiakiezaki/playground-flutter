1eb3ac181417d8bfae62acf9bb1b1f28c474173a
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2024-05-10 22:43:26 -0300

    Initialized Firebase into the app

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
-
-// TODO(codelab user): Get API key
-const clientId = 'YOUR_CLIENT_ID';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
