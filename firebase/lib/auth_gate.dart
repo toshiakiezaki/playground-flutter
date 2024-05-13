5aa709ebb7da70f137ae00f8d8f212d45e6f8688
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2024-05-12 22:07:09 -0300

    Added Google authentication service

--- firebase/lib/auth_gate.dart
+++ firebase/lib/auth_gate.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
 
 import 'home.dart';
@@ -16,6 +17,10 @@ class AuthGate extends StatelessWidget {
           return SignInScreen(
             providers: [
               EmailAuthProvider(),
+              GoogleProvider(
+                clientId:
+                    '401675165745-rev9rffrgnekpqgdspu2q94u1nng1cjb.apps.googleusercontent.com',
+              ),
             ],
             headerBuilder: (context, constraints, shrinkOffset) {
               return Padding(
